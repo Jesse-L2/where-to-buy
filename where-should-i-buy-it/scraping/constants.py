d71fb6e989b61b4340e1b4987a5fbcf61dcd5e65
Jesse-L2 <86895197+Jesse-L2@users.noreply.github.com>
2022-05-23 12:40:02 -0500

    Initial upload

---
+++ where-should-i-buy-it/scraping/constants.py
@@ -0,0 +1,7 @@
+websites = {
+            'AMAZON': "https://www.amazon.com",
+            'WALMART': "https://www.walmart.com",
+            'TARGET': "https://www.target.com",
+            'BEST_BUY': "https://www.bestbuy.com",
+
+}
