3d21ef77343bfc76207bd1513567d79f2f478ec3
Jesse-L2 <jesse.little@utexas.edu>
2022-06-11 14:31:40 -0500

    Added bestbuy_spider and target_spider folders. Updated pipelines.py.

--- wheretobuy/wheretobuy/pipelines.py
+++ wheretobuy/wheretobuy/pipelines.py
@@ -5,9 +5,40 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
+
 from itemadapter import ItemAdapter
 
 
-class WheretobuyPipeline:
+"""Scraped Data --> Item Containers --> Pipeline --> SQL or MongoDB database"""
+
+class WheretobuyPipeline(object):
+
+    def __init__(self):
+        # Automatically create connection and table when pipeline is created
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.connection = sqlite3.connect("prices.db")
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""DROP TABLE IF EXISTS prices_db""")
+        self.cursor.execute("""create table prices_db(
+                                title text,
+                                price real,
+        
+                            """)
+
+    def store_db(self, item):
+        self.cursor.execute("""insert into prices_db values (?, ?, ?)"""),
+        # needs more code to add item
+
+
+
+        self.connection.commit()
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
